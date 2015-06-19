64aac069d93a4183389c77754381870712a27b20
vova90 <vova__1990@ukr.net>
2015-06-19 11:55:34 +0300

    Add construc method to Human class

--- example7_1.py
+++ example7_1.py
@@ -3,15 +3,19 @@ class Human:
 	sex=None;
 	birth_year=None;
 	
+	def __init__(self, name, year):
+		self.name=name;
+		self.birth_year=year;
+	
 	def get_age(self):
 		if type (self.birth_year)==int and self.birth_year>1850:
 			return 2015-self.birth_year;
 		return None
-		
+			
 class Student:
 	pass
 
-vova=Human();
-ilona=Human();
-sasha=Human();	
-sveta=Human();
+vova=Human("Volodymyr", 1990);
+ilona=Human("Ilona", 1985);
+sasha=Human("Olexandr", 1963);
+sveta=Human("Svitlana", 1964);
