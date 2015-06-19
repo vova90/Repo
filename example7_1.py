0868f3873692ef4eec1d0d1868285d1efef5bb29
vova90 <vova__1990@ukr.net>
2015-06-19 11:53:18 +0300

    Add method get_age to Human class

--- example7_1.py
+++ example7_1.py
@@ -2,7 +2,12 @@ class Human:
 	name=None;
 	sex=None;
 	birth_year=None;
-
+	
+	def get_age(self):
+		if type (self.birth_year)==int and self.birth_year>1850:
+			return 2015-self.birth_year;
+		return None
+		
 class Student:
 	pass
 
