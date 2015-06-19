0d432d27f2797d41bf757ea7f16d43731bfa5037
vova90 <vova__1990@ukr.net>
2015-06-19 12:01:40 +0300

    Add atributes to constructor method to Human class and add methods say and say_parents to Human class

--- example7_1.py
+++ example7_1.py
@@ -2,20 +2,38 @@ class Human:
 	name=None;
 	sex=None;
 	birth_year=None;
+	mother=None;
+	father=None;
 	
-	def __init__(self, name, year):
+	def __init__(self, name, year, mother=None, father=None):
 		self.name=name;
 		self.birth_year=year;
+		self.mother=mother;
+		self.father=father;
 	
 	def get_age(self):
 		if type (self.birth_year)==int and self.birth_year>1850:
 			return 2015-self.birth_year;
 		return None
+		
+	def say_parents(self):
+		p1=''
+		if self.mother:
+			p1='mother is '+ self.mother.name
+		p2=""
+		if self.father:
+			p2='father is ' + self.father.name
+		if p1 and p2:
+			return 'My ' +p1 + ' and my ' + p2
+		return 'My ' + p1 + p2
+	
+	def say(self, message):
+		return self.name + ' says: "' + message + '".'
 			
 class Student:
 	pass
 
-vova=Human("Volodymyr", 1990);
-ilona=Human("Ilona", 1985);
 sasha=Human("Olexandr", 1963);
 sveta=Human("Svitlana", 1964);
+vova=Human("Volodymyr", 1990, sveta, sasha);
+ilona=Human("Ilona", 1985, None, sasha);
